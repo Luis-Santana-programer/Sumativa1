d34078617e6dfd945b08f9f00a804e00f2e84aa6
Luis S <11137@portalcei.edu.mx>
2022-01-21 10:10:04 -0600

    iniciando

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+    children: [
+  {
+    path: 'carro',
+    loadChildren: () => import('./carro/carro.module').then( m => m.CarroPageModule)
+  },
+  {
+    path: 'perfil',
+    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
+  },
+  {
+    path: 'compras',
+    loadChildren: () => import('./compras/compras.module').then( m => m.ComprasPageModule)
+  }
+  ]
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomePageRoutingModule {}
