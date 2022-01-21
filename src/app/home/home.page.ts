8690e14236a9eb898674f02fd84c86f6d181b995
Luis S <11137@portalcei.edu.mx>
2022-01-21 10:46:45 -0600

    Generar primer storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
-
+export class HomePage implements OnInit {
+  directorio: any;
+  producto: number;
+  usuario: boolean;
+  nusuario: number;
+  constructor(private storage: Storage) {
+    this.producto=0;
+    this.usuario=false;
+  }
+  async ngOnInit(){
+    this.storage=new Storage();
+    await this.storage.create();
+    //this.storage.clear();
+    this.directorio=await this.storage.get('directorio');
+    if(this.directorio==null){
+      for (let index = 0; index < 15; index++) {
+        this.directorio.push(this.producto);
+      }
+      this.directorio.push(this.usuario);
+      this.directorio.push(this.nusuario);
+      this.storage.set('directorio',this.directorio);
+    }
+  }
 }
